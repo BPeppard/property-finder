116016464dbc14febad0e76f69cf41f37909d1ec
Brian Peppard <BJPeppard@gmail.com>
2016-03-31 07:50:07 -0500

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+import SearchPage from './SearchPage'
+
+class PropertyFinder extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage
+        }}/>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
